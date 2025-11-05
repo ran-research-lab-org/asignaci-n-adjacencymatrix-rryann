9a3464daa45bd9f7b8d500b0ebd35819814bab84
Ryan Rolón <ryan.rolon@upr.edu>
2025-11-05 19:14:08 -0400

    Asignación completado

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ int main() {
   Graph G(10);
   G.addEdge(0,9);
   G.addEdge(4, 2);
+  G.addEdge(2, 4);
 
   G.print();
+  cout << "\nNum de aristas: " << G.numEdges() << endl;
+
+  cout << "In-degree de " << 4 << ": " << G.inDegree(4) << endl;
+
+  cout << "Is " << 2 << " influencer: " << G.isInfluencer(2) << endl;
+
+  return 0;
 }
